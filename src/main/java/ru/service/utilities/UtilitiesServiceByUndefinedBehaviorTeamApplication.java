dcc3ce3dc53520534a67e4d74e13a490fb8bbd0a
crazyfrequency <di.efimoff2015@gmail.com>
2025-02-28 17:21:26 +0400

    initial commit

---
+++ src/main/java/ru/service/utilities/UtilitiesServiceByUndefinedBehaviorTeamApplication.java
@@ -0,0 +1,13 @@
+package ru.service.utilities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilitiesServiceByUndefinedBehaviorTeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtilitiesServiceByUndefinedBehaviorTeamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/service/utilities/UtilitiesServiceByUndefinedBehaviorTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.service.utilities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilitiesServiceByUndefinedBehaviorTeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
