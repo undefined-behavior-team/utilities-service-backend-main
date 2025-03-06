09429e2bbb68f5ed57db4030e46cf0dc97b16434
Artem <mrfoxm06@gmail.com>
2025-03-06 23:04:24 +0400

    fix cache + redis

--- src/main/java/ru/service/utilities/UtilitiesServiceByUndefinedBehaviorTeamApplication.java
+++ src/main/java/ru/service/utilities/UtilitiesServiceByUndefinedBehaviorTeamApplication.java
@@ -2,8 +2,10 @@ package ru.service.utilities;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class UtilitiesServiceByUndefinedBehaviorTeamApplication {
 
 	public static void main(String[] args) {
